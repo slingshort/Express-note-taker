f7105e9b8b1ebc54244c0cf075b06c59f2927fa1
slingshort <salina.myat@gmail.com>
2022-10-21 15:21:14 +1100

    adding server.js file, still much troubleshooting required

--- server.js
+++ server.js
@@ -1,23 +1,79 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const uuid = require('./helpers/uuid');
+const notes = require('./db/db.json');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.port || 3001;
 
+// middlewards
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// home page
 app.get('/', (req, res) => 
-  res.send(path.join(__dirname,'/index.html'))
+  res.send(path.join(__dirname,'/public/index.html'))
   );
 
-// app.get('/home', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/index.html'))
-// );
-
+// notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname,'/notes.html'))
+  res.sendFile(path.join(__dirname,'/public/notes.html'))
 );
 
+// api get
+app.get('/api/notes', (req,res) => {
+  res.status(200).json(notes);
+ 
+  console.info(`${req.method} request recieved to get notes`);
+
+});
+
+// api post
+app.post('api/notes', (req, res) => {
+  console.info(`${req.method} request recieved to add a note`)
+
+  const {title, text} = req.body;
+
+  if(title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    fs.readFile(notes, 'utf-8', (err, data) => {
+      if(err) {
+        console.error(err);
+      } else {
+        console.log('connection made')
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        fs.writeFile(
+          'notes',
+          JSON.stringify(parsedNotes, null, 3),
+          (err) =>
+            err
+            ? console.error(err)
+            : console.info('Successfully added note.')
+            
+        );
+      } 
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,    
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in post');
+  }
+})
 
 
 app.listen(PORT, () =>
